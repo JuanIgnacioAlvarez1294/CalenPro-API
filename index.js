5a1777f0d11a87f3f5413005c6464f2983edbfc1
Juan Ignacio Alvarez <nacho1294.ji@gmail.com>
2025-03-25 17:51:43 -0300

    React App desplegada

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
@@ -22,7 +24,9 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
-
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
